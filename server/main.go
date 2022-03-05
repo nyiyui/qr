acba73be05b88d83f6005f93a2cd1d1d80f0eb5a
Ken Shibata <kenxshibata@gmail.com>
2022-03-05 11:33:15 -0500

    base64 support

--- server/main.go
+++ server/main.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"bytes"
+	"encoding/base64"
 	"fmt"
 	"net/http"
 
@@ -23,6 +24,23 @@ func New() *Server {
 		q, err := qrcode.New(src, qrcode.Medium)
 		if err != nil {
 			c.String(http.StatusInternalServerError, fmt.Sprint(err))
+			return
+		}
+		q.DisableBorder = true
+		buf := new(bytes.Buffer)
+		q.Write(256, buf)
+		c.DataFromReader(http.StatusOK, int64(buf.Len()), "image/png", buf, map[string]string{})
+	})
+	s.r.GET("/b64/:src", func(c *gin.Context) {
+		src, err := base64.URLEncoding.DecodeString(c.Param("src"))
+		if err != nil {
+			c.String(http.StatusBadRequest, fmt.Sprint(err))
+			return
+		}
+		q, err := qrcode.New(string(src), qrcode.Medium)
+		if err != nil {
+			c.String(http.StatusInternalServerError, fmt.Sprint(err))
+			return
 		}
 		q.DisableBorder = true
 		buf := new(bytes.Buffer)
